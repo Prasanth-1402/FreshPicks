e6dbfd959f8cec11f10afc60440339a19f0c2b47
Prasanth S <prasanthshanmugam1402@gmail.com>
2025-12-26 17:07:00 +0000

    issue fix

--- functions/routes/payments/index.js
+++ functions/routes/payments/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(
-  'sk_test_51I6yWmGAwh0uRQFn337V0Rn7d4JimqOsBZoljUp2RnyQruGCI4hI8ejkZfmaghnH7gStF7Ua1hmnKvbIzdsrPo2Y00JhjP1alH'
-);
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 // Create payment intent
 router.post('/create', async (req, res) => {
